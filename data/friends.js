b4a06b623b40c9ff44cd1b80209c26c7e932be7c
Wood Jerry Remelus <woodjremelus@gmail.com>
2018-04-26 04:41:23 -0600

    First commit

---
+++ data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Cindy Bunnell',
+		photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/10599660_10153132608471665_6184369095542758402_n.jpg?_nc_cat=0&oh=2e33e167b67604bfd81e0ec11dfe7657&oe=5B579BC8',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Luke Warner',
+		photo: 'https://www.facebook.com/photo.php?fbid=621658601498831&set=a.621658614832163.1073741828.100009640891861&type=3',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Wood J',
+		photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/18952756_1606787562678683_27135866041680999_n.jpg?_nc_cat=0&oh=cb6486fa5deb4a2f8a719b06da4113e2&oe=5B6968BD',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Lex Bsutista',
+		photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/13524554_10207780554926525_8159387632283044031_n.jpg?_nc_cat=0&oh=cf74655e1998378a2855fc539f324144&oe=5B66EEBC',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jean Baptiste',
+		photo: 'https://www.facebook.com/photo.php?fbid=123328914391888&set=a.111784375546342.11652.100001443404670&type=3',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Fred Mazile',
+		photo: 'https://www.facebook.com/photo.php?fbid=580703965347711&set=a.256166941134750.63608.100002243374534&type=3',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Suprinord Alexis',
+		photo: 'https://www.facebook.com/photo.php?fbid=580703965347711&set=a.256166941134750.63608.100002243374534&type=3',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Lexi Warner',
+		photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/18881966_10155309408398563_1636764926502944490_n.jpg?_nc_cat=0&oh=2b6d6bde9d5b86cc8b365fd5c995c07b&oe=5B5B7923',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jaycee Maria',
+		photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/24909942_1539579796128103_4901453419421892897_n.jpg?_nc_cat=0&oh=0620c613011a64eac8a06b44bab49b5d&oe=5B51BF2E',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Jaz Wil',
+		photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/30124840_1879934755638974_5295991372045465565_n.jpg?_nc_cat=0&oh=d99bf12b466b690b8c7a8d4c02e75725&oe=5B6AA739',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
